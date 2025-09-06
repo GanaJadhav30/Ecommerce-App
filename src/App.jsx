ce41061da7f6cc8cda28b42e67fb749275a145f9
GanaJadhav30 <jadhavganeshatclg@gmail.com>
2025-09-06 23:38:54 +0530

    Navbar complete

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,13 @@ import Cart from './pages/Cart'
 import Login from './pages/Login'
 import PlaceOrder from './pages/PlaceOrder'
 import Order from './pages/Order'
-
+import About from './pages/About'
+import Navbar from './components/Navbar'
 const App = () => {
   return (
-    <div className='px-4'>
-
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+    
+    <Navbar />
 
     <Routes>
       <Route path='/' element={<Home/>} />
